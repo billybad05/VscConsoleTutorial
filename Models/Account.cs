455489bdc76b3f32a4fb8447b220a9258f96bdba
Billy Dotson <billybad5@gmail.com>
2022-09-26 11:21:19 -0400

    Intial commit

---
+++ Models/Account.cs
@@ -0,0 +1,28 @@
+namespace Banking.Models;
+
+public class Account {
+    public int AccountNo { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public decimal Balance { get; set; }
+
+    public bool Deposit(decimal Amount) {
+            Balance += Amount;
+            return true;
+    }
+    public bool Withdraw(decimal Amount) {
+            if (Amount <= Balance) {
+                Balance -= Amount;
+                return true;
+            } else {
+                Console.WriteLine("Insufficient funds!");
+            }
+            return false;
+    }
+    public bool Transfer(decimal Amount, Account ToAccount) {
+            bool success = Withdraw(Amount);
+            if (success) {
+                success = ToAccount.Deposit(Amount);
+            }
+            return success;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello, World!");
+
+int i = 0;
+
+i += 5;
+
+System.Console.WriteLine(i);
